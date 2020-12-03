4afb88ab84ad92a8aaf8453fbad5443a25ab3b36
herman <yermanmicha@gmail.com>
2020-12-02 21:00:03 -0500

    cambio de año

--- src/test/java/co/edu/cotecnova/holamundo/HolaMundoApplicationTest.java
+++ src/test/java/co/edu/cotecnova/holamundo/HolaMundoApplicationTest.java
@@ -2,6 +2,6 @@ package test.java.co.edu.cotecnova.holamundo;
 
 public class HolaMundoApplicationTest {
     public static void main(String[]args){
-        System.out.println("hola mundo");
+        System.out.println("hola mundo 2020");
     }
 }
